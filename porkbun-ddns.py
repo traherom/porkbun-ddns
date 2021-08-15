2b03d69d5869e1edf1e39574592371084fefa046
Ryan <ryan@moreharts.com>
2021-08-15 06:34:29 -0600

    If existing record matches IP, don't update

--- porkbun-ddns.py
+++ porkbun-ddns.py
@@ -22,35 +22,38 @@ def get_records(config, domain):
             data=json.dumps(config)
         ).text
     )
+
     if resp["status"] == "ERROR":
         LOG.error(
             'Error getting domain. Check to make sure you specified the correct'
             'domain, and that API access has been switched on for this domain.'
         )
         sys.exit()
+
     LOG.debug("Response: %s", resp)
     return resp["records"]
 
 
-def get_my_ip(config):
+def get_my_ip(config) -> str:
     LOG.info("Getting external IP address")
-    ping = json.loads(requests.post(config["endpoint"] + '/ping/', data=json.dumps(config)).text)
+    ping = json.loads(
+        requests.post(
+            f"{config['endpoint']}/ping",
+            data=json.dumps(config)
+        ).text
+    )
     LOG.debug("Response: %s", ping)
     return ping["yourIp"]
 
 
-def delete_record(config, root_domain, sub_domain):
-    LOG.info("Removing domain if it already exists")
-    for i in get_records(config, root_domain):
-        if i["name"].startswith(sub_domain) and i["type"] in ('A', 'ALIAS', 'CNAME'):
-            LOG.info("Deleting existing " + i["type"] + " Record")
-            resp = json.loads(
-                requests.post(
-                    f"{config['endpoint']}/dns/delete/{root_domain}/{i['id']}",
-                    data=json.dumps(config)
-                ).text
-            )
-            LOG.debug("Response: %s", resp)
+def delete_record(config, root_domain, record_id):
+    resp = json.loads(
+        requests.post(
+            f"{config['endpoint']}/dns/delete/{root_domain}/{record_id}",
+            data=json.dumps(config)
+        ).text
+    )
+    LOG.debug("Response: %s", resp)
 
 
 def create_record(config, root_domain, sub_domain, ip):
@@ -80,35 +83,23 @@ def main():
     sub_domain = ".".join(domain_parts[:-2])
 
     ip = get_my_ip(config)
-    delete_record(config, root_domain, sub_domain)
-    create_record(config, root_domain, sub_domain, ip)
+
+    # Is the IP already correct?
+    new_record_needed = True
+    records = get_records(config, root_domain)
+    for record in records:
+        if record["name"].startswith(sub_domain) and record["type"] in ('A', 'ALIAS', 'CNAME'):
+            if record["content"] == ip:
+                LOG.info("Existing record is correct: %s %s", record["type"], record["content"])
+                new_record_needed = False
+                continue
+            
+            LOG.info("Existing record out of date, removing: %s %s", record["type"], record["content"])
+            delete_record(config, root_domain, record["id"])
+    
+    if new_record_needed:
+        create_record(config, root_domain, sub_domain, ip)
 
 
 if __name__ == "__main__":
     sys.exit(main())
-
-# if len(sys.argv) > 2:  # at least the config and root domain is specified
-#     apiConfig = json.load(open(sys.argv[1]))  # load the config file into a variable
-#     rootDomain = sys.argv[2]
-#
-#     if len(sys.argv) > 3 and sys.argv[3] != '-i':  # check if a subdomain was specified as the third argument
-#         subDomain = sys.argv[3]
-#         fqdn = subDomain + "." + rootDomain
-#     else:
-#         subDomain = ''
-#         fqdn = rootDomain
-#
-#     if len(sys.argv) > 4 and sys.argv[
-#         3] == '-i':  # check if IP is manually specified. There's probably a more-elegant way to do this
-#         myIP = sys.argv[4]
-#     elif len(sys.argv) > 5 and sys.argv[4] == '-i':
-#         myIP = sys.argv[5]
-#     else:
-#         myIP = getMyIP()  # otherwise use the detected exterior IP address
-#
-#     deleteRecord()
-#     print(createRecord()["status"])
-#
-# else:
-#     print(
-#         "Porkbun Dynamic DNS client, Python Edition\n\nError: not enough arguments. Examples:\npython porkbun-ddns.py /path/to/config.json example.com\npython porkbun-ddns.py /path/to/config.json example.com www\npython porkbun-ddns.py /path/to/config.json example.com '*'\npython porkbun-ddns.py /path/to/config.json example.com -i 10.0.0.1\n")
